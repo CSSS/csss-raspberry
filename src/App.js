c60da93767c18a76e977e14eed724395d0792f44
Micah Baker <micahb923@gmail.com>
2024-04-26 11:39:33 -0700

    Reorganise files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'csss-raspberry';
+
+export function App() {
+  return (
+    <>
+      <p>Hello, devs!</p>
+      <p>Use the src/App.js file to test your components.</p>
+      <Button>This is imported from lib/</Button>
+    </>
+  );
+}
