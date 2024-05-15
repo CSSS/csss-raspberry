5d2316d1eb5b84c033ecbbc7d3f9f9000132c602
Micah Baker <micahb923@gmail.com>
2024-05-15 12:33:50 -0700

    Use Hash Routing from react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,86 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { VSCode } from 'csss-raspberry';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import './index.css';
+import { VSCodeGuide } from './pages';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <VSCode.Navbar />
+        <VSCodeGuide />
+      </>
+    )
+  },
+  {
+    path: '/projects',
+    element: (
+      <>
+        <VSCode.Navbar />
+        <p>projects</p>
+      </>
+    )
+  },
+  {
+    path: '/about',
+    element: (
+      <>
+        <VSCode.Navbar />
+        <p>about</p>
+      </>
+    )
+  },
+  {
+    path: '/officers',
+    element: (
+      <>
+        <VSCode.Navbar />
+        <p>officers</p>
+      </>
+    )
+  },
+  {
+    path: '/officers/past',
+    element: (
+      <>
+        <VSCode.Navbar />
+        <p>officers - past</p>
+      </>
+    )
+  },
+  {
+    path: '/documents/:document',
+    element: (
+      <>
+        <VSCode.Navbar />
+        <p>documents - something</p>
+      </>
+    )
+  },
+  {
+    path: '/events/:event',
+    element: (
+      <>
+        <VSCode.Navbar />
+        <p>events - something</p>
+      </>
+    )
+  },
+  {
+    path: '/more/:something',
+    element: (
+      <>
+        <VSCode.Navbar />
+        <p>more - something</p>
+      </>
+    )
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 );
